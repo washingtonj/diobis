682e94f8050bbe5c1d7d52e70fb5a194497ed443
Washington Junior <wwnjr@icloud.com>
2024-04-16 23:46:54 -0300

    fix description error

--- apps/client/src/routes/+page.server.ts
+++ apps/client/src/routes/+page.server.ts
@@ -3,12 +3,7 @@ import { GitHubJobRepositoryAdapter } from '$lib/server/adapters';
 
 async function getdata() {
   const adapter = new GitHubJobRepositoryAdapter();
-  const response = await adapter.getJobs();
-
-  return response.map((job) => ({
-    ...job,
-    description: job.description.slice(0, 100)
-  }));
+  return adapter.getJobs();
 }
 
 export const load: PageServerLoad = async () => {
